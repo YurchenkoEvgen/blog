07beb934d7a86d635dd194b81997c7f33380ee51
evgen <evgenijvy@gmail.com>
2023-04-06 14:09:50 +0300

    collection only without grant

--- app/src/ApiFilter/BlogPostFilters.php
+++ app/src/ApiFilter/BlogPostFilters.php
@@ -5,33 +5,16 @@ declare(strict_types = 1);
 namespace App\ApiFilter;
 
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
-use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
 use App\Entity\BlogPost;
 use Doctrine\ORM\QueryBuilder;
-use Symfony\Bundle\SecurityBundle\Security;
 
-class BlogPostFilters implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+class BlogPostFilters implements QueryCollectionExtensionInterface
 {
-    private bool $isAdmin;
-
-    public function __construct(private readonly Security $security)
-    {
-        $user = $this->security->getUser();
-        $this->isAdmin = $user && in_array('ROLE_ADMIN', $user->getRoles());
-    }
-
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
     {
-        if (BlogPost::class == $resourceClass && !$this->isAdmin) {
-            $queryBuilder->andWhere(sprintf("%s.active = 1", $queryBuilder->getRootAliases()[0]));
-        }
-    }
-
-    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, Operation $operation = null, array $context = []): void
-    {
-        if (BlogPost::class == $resourceClass && !$this->isAdmin) {
+        if (BlogPost::class == $resourceClass ) {
             $queryBuilder->andWhere(sprintf("%s.active = 1", $queryBuilder->getRootAliases()[0]));
         }
     }
